5c32a9576f62480ab5bfb10076239ed8631948a2
Tomasini Marcello <whitehatty@gmail.com>
2016-08-23 17:15:59 -0400

    error: reference to ‘Font’ is ambiguous

--- Programming-code/Chapter12/chapter.12.7.9-2.cpp
+++ Programming-code/Chapter12/chapter.12.7.9-2.cpp
@@ -69,7 +69,7 @@ try
 
     Text t(Point(150,150), "Hello, graphical world! ");
     win.attach(t);
-    t.set_font(Font::times_bold);
+    t.set_font(Graph_lib::Font::times_bold);
     t.set_font_size(20);
 
     Image ii(Point(100,50),"image.jpg"); // 400*212 pixel jpg
